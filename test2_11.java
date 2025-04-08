02c394ce4ecee1ac4aa64ab0290080421e09e071
Min <152293441+mincodin9@users.noreply.github.com>
2025-04-08 18:29:34 +0900

    컴파일에러 수정
    배열 선언할 때 크기 불일치
    +
    ArrayList를 사용하지 않고, max값만을 사용해서도 풀 수 있음

--- test2_11.java
+++ test2_11.java
@@ -1,16 +1,14 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class test2_11 {
+public class Main {
     public static int test(int n, int[][] nums){
         int answer = 0;
         ArrayList<Integer> stu = new ArrayList<>();
-        ArrayList<Integer> tmp = new ArrayList<>();
         int max = 0;
-        int temp = 0;
 
         for(int i=0; i<n; i++){
-            temp=0;
+            int temp=0;
             for(int j=0; j<n; j++){
                 for(int k=0; k<5; k++){
                     if(nums[i][k]==nums[j][k]) {
@@ -30,7 +28,7 @@ public class test2_11 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] nums = new int[n][n];
+        int[][] nums = new int[n][5];
         for(int i=0; i<n; i++){
             for(int j=0; j<5; j++){
                 nums[i][j] = sc.nextInt();
