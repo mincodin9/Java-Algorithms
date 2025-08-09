c703195005d8ad078abb6e5fe746ff5c7aad6d5d
Min <152293441+mincodin9@users.noreply.github.com>
2025-08-09 09:20:20 +0900

    03-04 연속 부분수열
    N개의 수로 이루어진 수열
    이 수열에서 연속부분수열의 합이 특정숫자 M이 되는 경우가 몇 번있는지 구하기
    
    중복for문
    1. 첫번째 for문: N보다 작을 때까지
    2. 두번째 for문: 합이 M보다 크거나 같을 때까지

---
+++ continuousSubsequence03_04.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class continuousSubsequence03_04 {
+    public static int Solution(int n, int k, int[] nums) {
+        int answer = 0;
+        int temp=0;
+
+        for(int i=0; i<n; i++){
+            temp=0;
+            for(int j=0; i+j<n; j++){
+                temp += nums[i+j];
+                if(temp==k) {
+                    answer++;
+                    break;
+                }
+                if(temp>k){
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] nums = new int[n];
+        for(int i=0; i<n; i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(Solution(n, k, nums));
+    }
+}
