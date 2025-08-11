f212d845aa4b07b03786c02840614472673c2676
Min <152293441+mincodin9@users.noreply.github.com>
2025-08-12 08:33:25 +0900

    Update 04-04 모든 아나그램 찾기
    S문자열에서 T문자열과 아나그램이 되는 S의 부분문자열의 개수를 구하는 프로그램
    아나그램 판별시 대소문자 구분됨. 부분문자열은 연속된 문자열이어야 함
    
    1. char[] 입력받기
    2. s2의 크기만큼 sliding window 크기 설정
    3. s1의 sliding window와 s2가 아나그램인지 확인
    	3-1. HashMap<char, Integer><문자, 갯수>
    	3-2. 문자별 갯수 비교, 맞으면 int answer++
    4. lt, rt 증가 후 다시 비교(rt<s1.length)
    	4-1. lt증가 HashMap의 값 -1
    	4-2. rt증가 HashMap의 값 +1

--- hashMap/hashMap04_04.java
+++ hashMap/hashMap04_04.java
@@ -10,34 +10,18 @@ public class hashMap04_04 {
         HashMap<Character, Integer> map2 = new HashMap<>();
 
         //초기값 세팅
-        for(int i=0; i<sw; i++){
-            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
-            map2.put(arr2[i], map2.getOrDefault(arr2[i], 0)+1);
-        }
-
-        for(char c : map.keySet()){
-            if(map.get(c)!=map2.get(c)) break;
-            cnt++;
-            if(cnt==map2.size()) answer++;
-        }
+        for(int i=0; i<sw; i++) map2.put(arr2[i], map2.getOrDefault(arr2[i], 0)+1);
+        for(int i=0; i<sw-1; i++) map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
 
         int lt=0;
-        for(int rt=sw; rt<arr.length; rt++){
-            //lt, rt 값 증가
+        for(int rt=sw-1; rt<arr.length; rt++){
+            map.put(arr[rt], map.getOrDefault(arr[rt], 0)+1);
+            if(map.equals(map2)) answer++;
+
             map.put(arr[lt], map.get(arr[lt])-1);
             if(map.get(arr[lt])==0) map.remove(arr[lt]);
             lt++;
-            map.put(arr[rt], map.getOrDefault(arr[rt], 0)+1);
 
-            //아나그램 비교
-            cnt=0;
-            for(char c : map.keySet()){
-                if(map.get(c)!=map2.get(c)) {
-                    break;
-                }
-                cnt++;
-                if(cnt==map2.size()) answer++;
-            }
         }
 
         return answer;
