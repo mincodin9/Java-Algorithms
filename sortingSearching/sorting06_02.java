071ffd2bd80abfb075ad7db2f2c04b55daea2c60
Min <152293441+mincodin9@users.noreply.github.com>
2025-08-17 08:06:25 +0900

    06-02 Implement Bubble Sort
    - Compare adjacent elements and swap if needed
    - Repeat passes until array is sorted
    - Return sorted array in ascending order
    - Time O(n²), Space O(1)

---
+++ sortingSearching/sorting06_02.java
@@ -0,0 +1,32 @@
+package sortingSearching;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class sorting06_02 {
+    public static int[] Solution(int n, int[] arr){
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<n-i-1; j++){
+                if(arr[j]>arr[j+1]){
+                    int tmp=0;
+                    tmp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=tmp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int x : Solution(n, arr)){
+            System.out.print(x + " ");
+        }
+    }
+}
