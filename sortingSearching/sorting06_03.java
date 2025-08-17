b76acc48b50d724698c999380456ac52373a707d
Min <152293441+mincodin9@users.noreply.github.com>
2025-08-17 09:29:07 +0900

    06-03 Implement Insertion Sort
    - Iterate from i=1 to n-1
    - Store current element (tmp) and shift larger elements right
    - Insert tmp at the correct position
    - Return array sorted in ascending order
    - Time O(n²), Space O(1)

---
+++ sortingSearching/sorting06_03.java
@@ -0,0 +1,32 @@
+package sortingSearching;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class sorting06_03 {
+    public static int[] Solution(int n, int[] arr){
+
+        for(int i=1; i<n; i++){
+            int tmp = arr[i], j;
+            for(j=i-1; j>=0; j--){
+                if(arr[j]>tmp) arr[j+1]=arr[j];
+                else break;
+            }
+            arr[j+1]=tmp;
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int x : Solution(n, arr)){
+            System.out.print(x + " ");
+        }
+    }
+}
