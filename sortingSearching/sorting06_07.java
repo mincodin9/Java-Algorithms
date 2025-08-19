3b62440063d8b98b49e64c825e95d3ae8df69eb5
Min <152293441+mincodin9@users.noreply.github.com>
2025-08-19 09:50:34 +0900

    06-07 Sort coordinates with Comparable
    - Define Point class with x, y
    - Implement Comparable<Point>:
      - Primary key: x ascending
      - Secondary key: y ascending
    - Read n points, store in list
    - Sort using Collections.sort()
    - Print sorted coordinates

---
+++ sortingSearching/sorting06_07.java
@@ -0,0 +1,34 @@
+package sortingSearching;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+class Point implements Comparable<Point>{
+    public int x, y;
+    Point(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+    @Override
+    public int compareTo(Point o) {
+        if(this.x==o.x) return this.y-o.y;
+        else return this.x-o.x;
+    }
+}
+
+public class sorting06_07 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Point> arr = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            arr.add(new Point(x, y));
+        }
+        Collections.sort(arr);
+        for(Point o: arr) System.out.println(o.x + " " + o.y);
+    }
+}
