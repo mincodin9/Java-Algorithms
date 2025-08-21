c5813e46d9b0f4efd2b261b7c6090a1736b52ca6
Min <152293441+mincodin9@users.noreply.github.com>
2025-08-21 11:49:22 +0900

    06-10 Maximize minimum distance using Binary Search
    - Sort stall positions
    - Binary search distance range (1 .. max position)
    - For each mid distance, Count how many horses can be placed
    - If count >= c, update answer and increase distance
    - Else decrease distance
    - Return largest feasible minimum distance

---
+++ sortingSearching/binarySearch06_10.java
@@ -0,0 +1,44 @@
+package sortingSearching;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class binarySearch06_10 {
+    public static int Count(int[] arr, int dist){
+        int cnt=1, ep=arr[0];
+        for(int i=1; i<arr.length; i++){
+            if(arr[i]-ep>=dist){
+                cnt++;
+                ep = arr[i];
+            }
+        }
+        return cnt;
+    }
+
+    public static int Solution(int n, int c, int[] arr){
+        int answer=0;
+        Arrays.sort(arr);
+        int lt=1;
+        int rt=arr[n-1];
+        while(lt<=rt){
+            int mid=(lt+rt)/2;
+            if(Count(arr, mid)>=c){
+                answer=mid;
+                lt=mid+1;
+            }else rt=mid-1;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int c = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(Solution(n, c, arr));
+    }
+}
