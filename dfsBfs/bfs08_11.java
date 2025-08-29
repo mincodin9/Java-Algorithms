a8842c43d9116df3b5fe1ac02f828d995e632a3f
Min <152293441+mincodin9@users.noreply.github.com>
2025-08-29 11:52:28 +0900

    08-11 BFS: shortest path length in 7x7 maze
    - BFS from (1,1), marking visited on board
    - Track distances in dis[][]; dis[nx][ny] = dis[x][y] + 1
    - Return dis[7][7] or -1 if unreachable
    - Time O(7*7), Space O(7*7)

---
+++ dfsBfs/bfs08_11.java
@@ -0,0 +1,53 @@
+package dfsBfs;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Point{
+    public int x, y;
+    Point(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+}
+public class bfs08_11 {
+    static int[] dx={-1, 0, 1, 0};
+    static int[] dy={0, 1, 0, -1};
+    static int[][] board, dis;
+    static int answer=0;
+
+    public static void BFS(int x, int y){
+        Queue<Point> q=new LinkedList<>();
+        q.offer(new Point(x, y));
+        board[x][y]=1;
+        while(!q.isEmpty()){
+            Point tmp=q.poll();
+            for(int i=0; i<4; i++){
+                int nx=tmp.x+dx[i];
+                int ny=tmp.y+dy[i];
+                if(nx>=1 && nx<=7 && ny>=1 && ny<=7 && board[nx][ny]==0){
+                    board[nx][ny]=1;
+                    q.offer(new Point(nx, ny));
+                    dis[nx][ny]=dis[tmp.x][tmp.y]+1;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        bfs08_11 T = new bfs08_11();
+        Scanner sc = new Scanner(System.in);
+        board=new int[8][8];
+        dis=new int[8][8];
+        for(int i=1; i<=7; i++){
+            for(int j=1; j<=7; j++){
+                board[i][j]=sc.nextInt();
+            }
+        }
+        T.BFS(1, 1);
+        if(dis[7][7]==0) System.out.println(-1);
+        else System.out.println(dis[7][7]);
+    }
+}
