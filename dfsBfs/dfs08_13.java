b58d9fb25d60920d48c5270239535775e8b435f9
Min <152293441+mincodin9@users.noreply.github.com>
2025-08-30 11:27:32 +0900

    08-13 DFS: count islands in n×n grid (8 directions)
    - Traverse grid; when a land cell (1) is found:
      - Increment answer
      - DFS to sink all connected land (8-direction adjacency)
    - Return number of islands

---
+++ dfsBfs/dfs08_13.java
@@ -0,0 +1,46 @@
+package dfsBfs;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class dfs08_13 {
+    static int[] dx={-1, -1, 0, 1, 1, 1, 0, -1};
+    static int[] dy={0, 1, 1, 1, 0, -1, -1, -1};
+    static int answer=0, n;
+    public void DFS(int x, int y, int[][] board){
+        for(int i=0; i<8; i++){
+            int nx=x+dx[i];
+            int ny=y+dy[i];
+            if(nx>=0 && nx<n && ny>=0 && ny<n && board[nx][ny]==1){
+                board[nx][ny]=0;
+                DFS(nx, ny, board);
+            }
+        }
+    }
+    public static void Solution(int[][] board){
+        dfs08_13 T = new dfs08_13();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(board[i][j]==1){
+                    answer++;
+                    board[i][j]=0;
+                    T.DFS(i, j, board);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        dfs08_13 T = new dfs08_13();
+        Scanner sc = new Scanner(System.in);
+        n=sc.nextInt();
+        int[][] arr = new int[n][n];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        Solution(arr);
+        System.out.println(answer);
+    }
+}
